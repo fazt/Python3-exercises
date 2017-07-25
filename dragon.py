acb0ce89395145422ce48fd338dd36ddb82cb486
fazt <fabianncdev@gmail.com>
2017-07-25 01:32:09 -0500

    dragon and jokes added

---
+++ dragon.py
@@ -0,0 +1,42 @@
+import random
+import time
+
+def displayIntro():
+    print('You are in a lad full of dragons. In front of you')
+    print('you see two caves. In one cave the dragon is friendly')
+    print('and will share his treasure with you. The other dragon')
+    print('is greedy and hungry, and will eat you on sight.')
+    print()
+
+def chooseCave():
+    cave = ''
+    while cave != '1' and cave != '2':
+        print('Wich cave will you go into? (1 o 2)')
+        cave = input()
+
+    return cave
+
+def checkCave(chosenCave):
+    print('You approach the cave...')
+    time.sleep(2)
+    print('It is dark and spooky...')
+    time.sleep(2)
+    print('A large dragon jumps out in front of you! He opens his jaws and ...')
+    print()
+    time.sleep(2)
+
+    friendlyCave = random.randint(1, 2)
+
+    if chosenCave == str(friendlyCave):
+        print('Gives you his treasure!')
+    else:
+        print('Gobbles you down in one bite!')
+
+playAgain = 'yes'
+
+while playAgain == 'yes' or playAgain == 'y':
+    displayIntro()
+    caveNumber = choseCave()
+    checkCave(caveNumber)
+    print('Do you want to play Again? (yes or no)')
+    playAgain = input()
---
+++ jokes.py
@@ -0,0 +1,18 @@
+print('What do you get when you cross a snowman with a vampire')
+input()
+print('Frosbite!')
+print()
+
+print('What do dentists call a astrounaut\' cavity?')
+input()
+print('A black hole!')
+print()
+
+print('Know know?')
+input()
+print("Who's there?")
+input()
+print('Interrupting cow')
+input()
+print('Interrupting cow wh', end='')
+print('-MOO!')
